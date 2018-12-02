8274faf1d2b1dd27953e7e7dc0bf83fcd13b0fdf
Ivan Garcia <ivanvlora@gmail.com>
2018-12-02 16:59:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const takeANumber = (line, customerName) => {
+<<<<<<< HEAD
   // Add customer to array
   line.push(customerName);
+=======
+
+  // Add customer to array
+  line.push(customerName);
+
+>>>>>>> 2d73a7f45065e12cdcc833da78ea3b108a310989
   // Return the string with the customer name and the length of the array.
 	return `Welcome, ${customerName}. You are number ${line.length} in line.`
 }
 
+<<<<<<< HEAD
 const nowServing = (line) => {
   // Save the customer to a variable.
   const nowServing = line[0];
@@ -18,4 +26,24 @@ const currentLine = (line) => {
   return (line.length > 0 ? `The line is currently: ${line.map((customer, i) => {
      return i + 1 === 1 ? `${i + 1}. ${customer}` : ` ${i + 1}. ${customer}`
   })}` : 'The line is currently empty.')
+=======
+
+const nowServing = (line) => {
+  if (line.length > 0) {
+    return `Currently serving ${line[0]}.`;
+    line.shift();
+  } else {
+    return 'There is nobody waiting to be served!'
+  }
+}
+
+const currentLine = (line) => {
+  if (line.length > 0) {
+    return `The line is currently: ${line.map((customer, i) => {
+       return `${i + 1}. ${customer}`;
+    })}, `
+  } else {
+    return 'The line is currently empty.'
+  }
+>>>>>>> 2d73a7f45065e12cdcc833da78ea3b108a310989
 }
